23a719ecc55806d4a62097181d761c9499f3ddff
lyq <liyaqi@1214.com>
2017-06-09 20:53:43 +0800

    引入模板引擎并实现讲师列表加载

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,17 +1,12 @@
-define(['jquery','template','bootstrap'],function($,template){
+define(['jquery','template'],function($,template){
 	//加载列表数据
 	$.ajax({
 		type : 'get',
 		url : '/api/teacher',
 		dataType : 'json',
 		success : function(data){
-			//解析数据并渲染页面
 			var html = template('teacherInfoTpl',{list:data.result});
 			$('#teacherInfo').html(html);
-			//查看讲师功能
-			previewTeacher();
-			//注销和启用讲师
-			enableOrDisableTeacher();
 		}
 	});
 });
