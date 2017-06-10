414c4550e85c4fe04611a7f4f79dea2e91209c2b
lyq <liyaqi@1214.com>
2017-06-10 13:53:08 +0800

    遮罩效果添加

--- public/js/common.js
+++ public/js/common.js
@@ -34,5 +34,14 @@ define(['jquery','template','cookie'],function($,template){
         // $('.aside .profile').find('h4').text(loginInfo.tc_name);
     }
 
-    
+    //加载遮罩效果
+    $(document).ajaxStart(function(){
+        //显示遮罩效果
+        $('.overlay').show();
+
+    })
+    $(document).ajaxStop(function(){
+        //隐藏遮罩效果
+        $('.overlay').hide();
+    });
 });
