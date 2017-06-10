6ea8fb715bc66f100851552982a015dec0ade685
lyq <liyaqi@1214.com>
2017-06-10 13:29:50 +0800

    设置侧边栏菜单选中

--- public/js/config.js
+++ public/js/config.js
@@ -7,6 +7,8 @@ requirejs.config({
         template : 'artTemplate/template-web',
         common : '../js/common',
         login : '../js/login',
+        index : '../js/index',
+        util : '../js/util',
         tealist : '../js/teacher-list'
     },
     shim : {// 兼容非标准模块
---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+define(['jquery','util'],function($,util){
+	//设置导航菜单选中
+	util.setMenu(location.pathname);
+	// var pathname = location.pathname;
+	// $('.aside .navs a').removeClass('active');
+	// $('.aside .navs a[href = "'+pathname+'"]').addClass('active');
+
+});
